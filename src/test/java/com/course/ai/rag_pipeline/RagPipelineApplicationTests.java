1ebd45013fff23182e025e17ef663959259f4672
ParamjotSingh5 <Paramjotdhiman5@gmail.com>
2025-06-18 23:52:29 +0530

    Intial commit

---
+++ src/test/java/com/course/ai/rag_pipeline/RagPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.ai.rag_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
