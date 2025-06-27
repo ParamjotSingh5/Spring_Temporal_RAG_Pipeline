9200c7cecd03bd03898b97938774fb2a5f7b756f
ParamjotSingh5 <Paramjotdhiman5@gmail.com>
2025-06-27 23:54:45 +0530

    updated config to include textsplitter into config

--- src/main/java/com/course/ai/rag_pipeline/config/AIConfig.java
+++ src/main/java/com/course/ai/rag_pipeline/config/AIConfig.java
@@ -2,6 +2,8 @@ package com.course.ai.rag_pipeline.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -18,5 +20,10 @@ public class AIConfig {
     ChatClient chatClient(ChatClient.Builder builder) {
         return builder.defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
+    }
+
+    @Bean
+    TextSplitter textSplitter() {
+        return new TokenTextSplitter();
     }
 }
